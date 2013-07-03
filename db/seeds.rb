938ed789dd62e10a1fb0967b64717b6720025372
supertopher <asimplechris@gmail.com>
2013-07-03 16:48:51 -0700

    SO much fake data!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 require 'faker'
 
 20.times do
-  Post.create(title: Faker::Company.bs, content: Faker::Lorem.sentences(4), url: Faker::Internet.url)
+  Post.create(title: Faker::Company.bs, content: Faker::Lorem.sentences(2), url: Faker::Internet.url)
+end
+
+20.times do
+  User.create(username: Faker::Name.name, email: Faker::Internet.email, password: "password")
+end
+
+user_rand = User.all.count
+post_rand = Post.all.count
+
+post_rand.times do |index|
+  rand(9).times do
+    Comment.create(body: Faker::Lorem.sentence(2), post_id: index, user_id: rand(user_rand), )
+  end
 end
